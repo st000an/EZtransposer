98bfaad01070669f0237651a4f1c69abb03ee465
st000an <st000an@gmail.com>
2025-03-01 15:49:38 +0800

    init

---
+++ main.py
@@ -0,0 +1,40 @@
+import librosa
+import librosa.display
+import soundfile as sf
+import numpy as np
+from pydub import AudioSegment
+
+AudioSegment.converter = r"D:\0-git\EZtransposer\ffmpeg-2025-02-26-git-99e2af4e78-full_build\ffmpeg-2025-02-26-git-99e2af4e78-full_build\bin\ffmpeg.exe"
+AudioSegment.ffprobe = r"D:\0-git\EZtransposer\ffmpeg-2025-02-26-git-99e2af4e78-full_build\ffmpeg-2025-02-26-git-99e2af4e78-full_build\bin\ffprobe.exe"
+
+import os
+
+def change_pitch(input_file, n_semitones, output_file="output.wav"):
+    """
+
+    :param input_file: str, 輸入音檔路徑 (支援 .wav 和 .mp3)
+    :param n_semitones: int, 轉調的半音數 (正數為升調，負數為降調)
+    :param output_file: str, 輸出 .wav 檔案名稱
+    """
+    temp_wav = None
+    if input_file.lower().endswith('.mp3'):
+        audio = AudioSegment.from_mp3(input_file)
+        temp_wav = "temp_conversion.wav"
+        audio.export(temp_wav, format="wav")
+        input_file = temp_wav
+
+    y, sr = librosa.load(input_file, sr=None)  
+
+    # 轉調
+    y_shifted = librosa.effects.pitch_shift(y, sr=sr, n_steps=n_semitones)
+
+    sf.write(output_file, y_shifted, sr)
+    if temp_wav and os.path.exists(temp_wav):
+        os.remove(temp_wav)
+
+    print(f"轉調完成，輸出檔案: {output_file}")
+
+
+inputAudio="D:\\0-git\\EZtransposer\\Sweet, Sexy Lover.mp3"
+change_pitch(inputAudio, -1, inputAudio+"_output.wav") 
+
